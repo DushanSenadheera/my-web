7a04718d655fe38cd6b07f1c131352650c8c0984
DushanSenadheera <89749333+DushanSenadheera@users.noreply.github.com>
2022-02-02 20:07:06 +0530

    Add files via upload

---
+++ insert.php
@@ -0,0 +1,25 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "login";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+$emai = $_REQUEST['email'];
+$password = $_REQUEST['password'];
+
+$sql = "INSERT INTO user (email,password)
+VALUES ('$emai', '$password')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
